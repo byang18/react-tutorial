dd3bb579e9fc09058bad54258cbacf8e8cae6d7a
Barry <barry.a.yang.18@dartmouth.edu>
2019-01-22 21:43:30 -0500

    set up

---
+++ src/code_editor.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import AceEditor from 'react-ace';
+
+class CodeEditor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+}
+
+export default CodeEditor;
