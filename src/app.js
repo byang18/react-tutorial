c97d33284f6e755a3ea49a2f725dfb8bc9bd8b0e
Barry <barry.a.yang.18@dartmouth.edu>
2019-01-22 23:00:18 -0500

    editor addd

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import CodeEditor from './code_editor';
+
+const App = () => {
+  return (
+    <div id="main-window">
+      <h1>Interactive React Tutorial</h1>
+      <CodeEditor />
+    </div>
+  );
+};
+
+export default App;
+
+// import React from 'react';
+//
+// const App = () => {
+//   return (
+//     <div id="main-window"> Hello World!</div>
+//   );
+// };
+//
+// export default App;
--- src/code_editor.js
+++ src/code_editor.js
@@ -1,10 +1,36 @@
+/* eslint-disable no-useless-escape */
+
 import React, { Component } from 'react';
 import AceEditor from 'react-ace';
 
 class CodeEditor extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      value:
+`import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>All the React are belong to us!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('main'));`,
+    };
+  }
+
+  handleChange = value => this.setState({ value })
+
+  render() {
+    const { value } = this.state;
+    return (
+      <AceEditor
+        mode="javascript"
+        value={value}
+        onChange={this.handleChange}
+        wrapEnabled
+      />
+    );
   }
 }
 
