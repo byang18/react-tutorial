2ddf9c1ba9f8ae56506405eb6414d721d5e3b4aa
Barry <barry.a.yang.18@dartmouth.edu>
2019-02-04 00:48:08 -0500

    render react component working

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,8 @@
-// const babel = require('babel-core').transform('code');
+/* eslint-disable no-unused-vars */
+
+import * as babel from 'babel-standalone';
+import React from 'react';
+import { render } from 'react-dom';
 
 export const add = (x, y) => {
   return x + y;
@@ -10,17 +14,37 @@ export const mutiply = (x, y) => {
 
 
 export const runCode = () => {
-//   const code = `import React from 'react';
-// import ReactDOM from 'react-dom';
-//
-// const App = () => {
-//   return <div>All the React are belong to us!</div>;
-// };
-//
-// ReactDOM.render(<App />, document.getElementById('main'));`;
+  const component = `
+  const App = () => {
+      return <div>All the React are belong to us!</div>;
+    };`;
 
-  const code = "document.getElementById('todo-container').innerHTML = 'hello!'";
+  const babelCode = babel.transform(component, { presets: ['react', 'es2017'] }).code;
+  const code = babelCode.replace('"use strict";', '').trim();
+  const newCode = `${code} \n return App;`;
+  console.log(newCode);
+  const func = new Function('React', `${code}\nreturn App;`);
+
+  // const App = () => {
+  //   return React.createElement(
+  //     'div',
+  //     null,
+  //     'All the React are belong to us!',
+  //   );
+  // };
+
+  // return function App() {
+  //   return React.createElement(
+  //     'div',
+  //     null,
+  //     'All the React are belong to us!',
+  //   );
+  // };
+
+  const App = func(React);
+  render(<App />, document.getElementById('todo-container'));
+
+  // const code = "document.getElementById('todo-container').innerHTML = 'hello!'";
 
   // const jsCode = babel.transform(code);
-  eval(code);
 };
